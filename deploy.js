5ecc2ed4e4e71cb0e74f6a2ec5912fc5b93ca796
jminango20 <jcarlosminango@gmail.com>
2022-11-24 22:21:05 -0300

    interacting with the smart contract

--- deploy.js
+++ deploy.js
@@ -9,14 +9,18 @@ async function main(){
     const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf-8");
     const binary = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.bin", "utf-8");
     const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+    //Deploy contract
     console.log("Deploying, please wait!...");
     const contract = await contractFactory.deploy(); //STOP here!. Wait for contract to deploy
-    const transactionReceipt = await contract.deployTransaction.wait(1);
-    console.log("Here is the deployment transaction (transaction response): ");
-    console.log(contract.deployTransaction);
-    
-    console.log("Here is the transaction receipt: ")
-    console.log(transactionReceipt);
+    await contract.deployTransaction.wait(1);
+    //Get Favorite Number by Interacting with the contract
+    const currentFavoriteNumber = await contract.retrieve();
+    console.log(`Your initial favorite number is: ${currentFavoriteNumber}`);
+    //Update Favorite Number by Interacting with the contract
+    const transactionResponse = await contract.store("7");
+    const transactionReceipt = await transactionResponse.wait(1);
+    const updateFavoriteNumber = await contract.retrieve();
+    console.log(`Your update favorite number is: ${updateFavoriteNumber}`);   
 
 }
 
